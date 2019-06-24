dc5bb802319b10380dda1dc85e39c8bd04328b49
Your Name <you@example.com>
2019-06-24 23:40:56 +0300

    remove redundant init methods.

--- pages/locators.py
+++ pages/locators.py
@@ -2,15 +2,9 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    def __init__(self):
-        pass
-
     LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
 
 
 class LoginPageLocators:
-    def __init__(self):
-        pass
-
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
