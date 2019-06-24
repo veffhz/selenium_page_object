ca4e7ff09a4045a676759d1bd6c4b760302009b6
Your Name <you@example.com>
2019-06-24 23:36:02 +0300

    include MainPage open() to login_page test.

--- test_main_page.py
+++ test_main_page.py
@@ -3,7 +3,6 @@ from .pages.login_page import LoginPage
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-login_link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -19,6 +18,10 @@ def test_guest_should_see_login_link(browser):
 
 
 def test_guest_should_be_login_page(browser):
-    page = LoginPage(browser, login_link)
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_page()
+    page.go_to_login_page()
+
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.open()
+    login_page.should_be_login_page()
