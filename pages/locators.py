8e757c949450918b168d5f5d087055b81e6f95e7
Your Name <you@example.com>
2019-06-26 23:56:26 +0300

    Removed MainPageLocators.

--- pages/locators.py
+++ pages/locators.py
@@ -6,10 +6,6 @@ class BasePageLocators(object):
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 
-class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
-
-
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
