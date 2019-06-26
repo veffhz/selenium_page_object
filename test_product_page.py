810b82c392572098ae3d1949eeba2e420e099a36
Your Name <you@example.com>
2019-06-26 21:53:36 +0300

    refactoring urls.

--- test_product_page.py
+++ test_product_page.py
@@ -4,7 +4,7 @@ from .pages.product_page import ProductPage
 
 product_link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 product_link2 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+
 
 
 def test_guest_can_add_product_to_cart(browser):
@@ -27,16 +27,8 @@ def test_guest_can_add_product2_to_cart(browser):
     page.should_be_product_added_to_basket()
 
 
-urls = [f"{product_base_link}/?promo=offer0",
-        f"{product_base_link}/?promo=offer1",
-        f"{product_base_link}/?promo=offer2",
-        f"{product_base_link}/?promo=offer3",
-        f"{product_base_link}/?promo=offer4",
-        f"{product_base_link}/?promo=offer5",
-        f"{product_base_link}/?promo=offer6",
-        f"{product_base_link}/?promo=offer7",
-        f"{product_base_link}/?promo=offer8",
-        f"{product_base_link}/?promo=offer9"]
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
 
 
 @pytest.mark.parametrize('link', urls)
